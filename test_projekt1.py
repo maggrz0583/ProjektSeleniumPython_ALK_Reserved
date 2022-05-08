9f4f1829c09407bbd7d6f4544b4ecbbb7ced1af0
Magdalena Niedziulka <m.grzejszczyk@onet.eu>
2022-05-08 21:25:50 +0200

    Test draft added

---
+++ test_projekt1.py
@@ -0,0 +1,17 @@
+#Import bibliotek
+from selenium import webdriver
+
+import unittest
+
+class TestRegistration(unittest.TestCase):
+    def setUp(self):
+        #przygotowanie testu
+        #otwarta strona https://www.douglas.pl/pl/login
+    def tearDown(self):
+        pass
+    def testInvalidEmail(self):
+        pass
+
+    if __name__== "__main__":
+
+       unittest.main()
