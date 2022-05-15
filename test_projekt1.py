e5ca149a7c22235ee3740023b6bd4e8b3b3b25b9
Magdalena Niedziulka <m.grzejszczyk@onet.eu>
2022-05-15 22:27:37 +0200

    TestCase modyfiaed added

--- test_projekt1.py
+++ test_projekt1.py
@@ -1,6 +1,9 @@
 #Import bibliotek
-from selenium import webdriver
+
 import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
 
 class TestRegistration(unittest.TestCase):
     def setUp(self):
@@ -8,16 +11,20 @@ class TestRegistration(unittest.TestCase):
         #1. Otwarta strona https://www.douglas.pl/pl
         self.driver = webdriver.Firefox()
         self.driver.maximize_window()
-        self.driver.get("https://www.douglas.pl/pl")
+        self.driver.get("https://www.sephora.pl/")
 
     def tearDown(self):
         # Zakończenie testu
-        self.driver.quit(15)
+        self.driver.quit()
 
     def testInvalidEmail(self):
-        #1.Kliknij utwórz konto - otwiera się strona rejestracji
-        driver.find.element(By.)
-#Jeśli uruchamiam ten plik
+        driver = self.driver
+        # Zaakceptuj popup
+        driver.implicitly_wait(15)
+        accept_btn = driver.find_element(By.ID,"footer_tc_privacy_button_2")
+        accept_btn.click()
+
+        #Jeśli uruchamiam ten plik
 if __name__== "__main__":
 #Uruchom testy
     unittest.main()
