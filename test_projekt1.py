a3ce3d2ceeafcc1ab2f9416829819f303346d6ea
Magdalena Niedziulka <m.grzejszczyk@onet.eu>
2022-05-16 21:48:15 +0200

    TestCase modyfiaed added

--- test_projekt1.py
+++ test_projekt1.py
@@ -11,32 +11,34 @@ from time import sleep
 class TestRegistration(unittest.TestCase):
     def setUp(self):
         #Przygotowanie testu
-        #1. Otwarta strona https://www.douglas.pl/pl
+        #1. Otwarta strona https://www.reserved.com/pl/pl/
         self.driver = webdriver.Firefox()
         self.driver.maximize_window()
-        self.driver.get("https://www.sephora.pl/")
+        self.driver.get("https://www.reserved.com/pl/pl/")
 
     def tearDown(self):
         # Zakończenie testu
         self.driver.quit()
 
-    def testInvalidEmail(self):
+    def testInvalidEmail(self, span=None):
         driver = self.driver
         # Zaakceptuj popup
         driver.implicitly_wait(15)
-        accept_btn = driver.find_element(By.ID,"footer_tc_privacy_button_2")
+        accept_btn = driver.find_element(By.ID,"cookiebotDialogOkButton")
         accept_btn.click()
 
         #Kliknij profil - otwiera się menu z opcjami zalogowania/utworzenia konta
-        profile = driver.find_element(By.LINK_TEXT, "Konto")
-        webdriver.ActionChains(driver).move_to_element(profile).perform()
+        profile_button = driver.find_element(By.XPATH, "/html/body/div[2]/div/div[2]/div/div[3]/div[2]")
+        driver.implicitly_wait(10)
+        #profile_button.click()
+        webdriver.ActionChains(driver).move_to_element(profile_button).perform()
 
         # Utwórz konto
-        registracion = driver.find_element(By.LINK_TEXT, "UTWÓRZ KONTO")
-        registracion.click()
-        #otwiera się strona rejestracji - NIE AŁA
-        self.assertIn("UTWÓRZ KONTO", driver.title)
-
+        register = driver.find_element(By.LINK_TEXT, "Zarejestruj się")
+        register.click()
+        #otwiera się strona rejestracji
+        #self.assertIn("Utwórz konto", driver.title)
+#
         # Jeśli uruchamiam ten plik
 if __name__ == "__main__":
          # Uruchom testy
